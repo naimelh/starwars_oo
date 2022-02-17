ba8adf63c512a3f2984e134d529836278c2ab4bd
TheJazzyBranco <naimelhammouchi@icloud.com>
2022-02-17 12:07:02 +0100

    chapter 4

---
+++ play.php
@@ -0,0 +1,69 @@
+<?php
+
+class Ship 
+
+{
+
+  public $name;
+
+  public  $weaponPower = 0;
+  public  $jediFactor = 0;
+  public  $strength = 0;
+
+
+  public function sayHello()
+  {
+    echo 'Hello!';
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+public function getNameAndSpecs($useShortFormat)
+{
+      if ($useShortFormat){
+        return sprintf(
+                    '%s: %s/%s/%s',
+                    $this->name,
+                    $this->weaponPower,
+                    $this->jediFactor,
+                    $this->strength,
+        );
+      } else{
+        return sprintf(
+          '%s: w:%s, j:%s, s:%s',
+          $this->name,
+          $this->weaponPower,
+          $this->jediFactor,
+          $this->strength
+);
+      }
+  
+}
+
+}
+
+function printShipSummary($someShip){
+
+  
+}
+
+
+
+$myShip = new Ship();
+$myShip ->name = 'Jedi Starship';
+$myShip -> weaponPower = 10;
+
+echo 'Ship name: ' .$myShip->name;
+echo '<hr/>';
+$myShip->sayHello();
+echo '<hr/>';
+echo $myShip->getName();
+echo '<hr/>';
+echo $myShip->getNameAndSpecs(false);
+echo '<hr/>';
+echo $myShip->getNameAndSpecs(true);
+
+?>
